e287370f0386e18ba279f218359ee5d574338653
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-06-29 16:49:07 -0400

    Create and Read crud functions set up

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Cards from "./dbCards.js"
 
 // App Config
 const app = express();
@@ -17,5 +18,26 @@ mongoose.connect(connection_url, {
 // API Endpoints
 app.get("/", (req, res) => res.status(200).send("MLEM"));
 
+app.post('/mlem/card', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
+
+app.get('/mlem/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
