02996451f1350f2c941f64c73231a30b3251f537
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-06-29 17:17:09 -0400

    used post method on postman to the api endpoint to add data to mongoDb

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 import Cards from "./dbCards.js"
 
 // App Config
 const app = express();
 const port = process.env.PORT || 8001
-const connection_url = `mongodb+srv://Ariel:all4ara94@cluster0.ccplu.mongodb.net/mlemdb?retryWrites=true&w=majority`
+const connection_url = `mongodb+srv://Ariel:all4ara94@cluster0.ccplu.mongodb.net/mlemdb?retryWrites=true&w=majority`;
+
 // Middlewares
+app.use(express.json());
+app.use(cors());
 
 // DB config
 mongoose.connect(connection_url, {
@@ -18,7 +22,7 @@ mongoose.connect(connection_url, {
 // API Endpoints
 app.get("/", (req, res) => res.status(200).send("MLEM"));
 
-app.post('/mlem/card', (req, res) => {
+app.post('/mlem/cards', (req, res) => {
     const dbCard = req.body;
 
     Cards.create(dbCard, (err, data) => {
